04bc571d4c11785aa8462e132fee531b9c824fd0
Tom Liu <tom.5610@gmail.com>
2022-05-28 00:11:33 +0000

    Glue workflow pipeline code.

---
+++ common/setup_iam_roles.py
@@ -0,0 +1,251 @@
+import json
+import boto3
+
+iam = boto3.client('iam')
+
+def create_glue_role(role_name, bucket):
+    try:
+        response = iam.create_role(
+            RoleName = role_name,
+            AssumeRolePolicyDocument = json.dumps({
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Effect": "Allow",
+                        "Principal": {
+                            "Service": "glue.amazonaws.com"
+                        },
+                        "Action": "sts:AssumeRole"
+                    }
+                ]
+            }),
+            Description='Role for Glue ETL job'
+        )
+        
+        role_arn = response['Role']['Arn']
+        
+        role_policy_document = json.dumps({
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Sid": "ListObjectsInBucket",
+                        "Effect": "Allow",
+                        "Action": ["s3:ListBucket"],
+                        "Resource": [f"arn:aws:s3:::{bucket}"]
+                    },
+                    {
+                        "Sid": "AllObjectActions",
+                        "Effect": "Allow",
+                        "Action": "s3:*Object",
+                        "Resource": [f"arn:aws:s3:::{bucket}/*"]
+                    },
+                    {
+                        "Sid": "SageMakerExecution",
+                        "Effect": "Allow",
+                        "Action": "sagemaker:*",
+                        "Resource": "*"
+                    },
+                    {
+                        "Action": [
+                            "logs:CreateLogStream",
+                            "logs:DescribeLogStreams",
+                            "logs:PutLogEvents",
+                            "logs:CreateLogGroup"
+                        ],
+                        "Resource": [
+                            "arn:aws:logs:*:*:log-group:/aws/sagemaker/*",
+                            "arn:aws:logs:*:*:log-group:/aws/sagemaker/*:log-stream:aws-glue-*"
+                        ],
+                        "Effect": "Allow"
+                    }                    
+                ]
+            })
+        
+        response = iam.put_role_policy(
+            RoleName=role_name,
+            PolicyName=f'{role_name}_S3SageMakerAccessPolicy',
+            PolicyDocument=role_policy_document
+        )
+        
+        return role_arn
+
+    except iam.exceptions.EntityAlreadyExistsException:
+        print(f'Using ARN from existing role: {role_name}')
+        response = iam.get_role(RoleName=role_name)
+        return response['Role']['Arn']
+
+def create_sfn_role(role_name, notebook_role_arn, glue_job_prefix):
+    try:
+        response = iam.create_role(
+            RoleName = role_name,
+            AssumeRolePolicyDocument = json.dumps({
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Effect": "Allow",
+                        "Principal": {
+                            "Service": "states.amazonaws.com"
+                        },
+                        "Action": "sts:AssumeRole"
+                    }
+                ]
+            }),
+            Description='Role for Lambda to step function'
+        )
+
+        role_arn = response['Role']['Arn']
+        
+        response = iam.attach_role_policy(
+            RoleName=role_name,
+            PolicyArn='arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole'
+        )
+        
+        response = iam.attach_role_policy(
+            RoleName=role_name,
+            PolicyArn='arn:aws:iam::aws:policy/CloudWatchFullAccess',
+        )
+        
+        role_policy_document = json.dumps({
+            "Version": "2012-10-17",
+            "Statement": [{
+                    "Effect": "Allow",
+                    "Action": "iam:PassRole",
+                    "Resource": notebook_role_arn,
+                    "Condition": {
+                        "StringEquals": {
+                            "iam:PassedToService": "sagemaker.amazonaws.com"
+                        }
+                    }
+                },
+                {
+                    "Effect": "Allow",
+                    "Action": [
+                        "sagemaker:CreateModel",
+                        "sagemaker:CreateProcessingJob",
+                        "sagemaker:CreateEndpointConfig",
+                        "sagemaker:DeleteEndpointConfig",
+                        "sagemaker:CreateEndpoint",
+                        "sagemaker:UpdateEndpoint",
+                        "sagemaker:DeleteEndpoint",
+                        "sagemaker:DescribeTrainingJob",
+                        "sagemaker:StopTrainingJob",
+                        "sagemaker:CreateTrainingJob"
+                    ],
+                    "Resource": [
+                        "arn:aws:sagemaker:*:*:*"
+                    ]
+                },
+                {
+                    "Effect": "Allow",
+                    "Action": [
+                        "events:DescribeRule",
+                        "events:PutRule",
+                        "events:PutTargets"
+                    ],
+                    "Resource": [
+                        "arn:aws:events:*:*:rule/*"
+                    ]
+                },
+                {
+                    "Effect": "Allow",
+                    "Action": [
+                        "lambda:InvokeFunction"
+                    ],
+                    "Resource": [
+                        f"arn:aws:lambda:*:*:*"
+                    ]
+                },
+                {
+                    "Effect": "Allow",
+                    "Action": [
+                        "glue:StartJobRun",
+                        "glue:GetJobRun",
+                        "glue:BatchStopJobRun",
+                        "glue:GetJobRuns"
+                    ],
+                    "Resource": f"arn:aws:glue:*:*:job/{glue_job_prefix}*"
+                }
+            ]
+        })
+        
+        response = iam.put_role_policy(
+            RoleName=role_name,
+            PolicyName=f'{role_name}SFNWorkflowExecutionPolicy',
+            PolicyDocument=role_policy_document
+        )
+        
+        return role_arn
+    except iam.exceptions.EntityAlreadyExistsException:
+        print(f'Using ARN from existing role: {role_name}')
+        response = iam.get_role(RoleName=role_name)
+        return response['Role']['Arn']
+
+def create_lambda_role(role_name, bucket):
+    try:
+        response = iam.create_role(
+            RoleName = role_name,
+            AssumeRolePolicyDocument = json.dumps({
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Effect": "Allow",
+                        "Principal": {
+                            "Service": "lambda.amazonaws.com"
+                        },
+                        "Action": "sts:AssumeRole"
+                    }
+                ]
+            }),
+            Description='Role for Lambda to step function'
+        )
+
+        role_arn = response['Role']['Arn']
+
+        response = iam.attach_role_policy(
+            RoleName=role_name,
+            PolicyArn='arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole'
+        )
+        
+        response = iam.attach_role_policy(
+            RoleName=role_name,
+            PolicyArn='arn:aws:iam::aws:policy/AmazonSageMakerFullAccess',
+        )
+
+        role_policy_document = json.dumps({
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Action": "s3:GetObject",
+                    "Resource": [f"arn:aws:s3:::{bucket}/*"]
+                }
+            ]
+        })
+
+        response = iam.put_role_policy(
+            RoleName=role_name,
+            PolicyName=f'{role_name}LambdaPolicy',
+            PolicyDocument=role_policy_document
+        )
+        return role_arn
+    except iam.exceptions.EntityAlreadyExistsException:
+        print(f'Using ARN from existing role: {role_name}')
+        response = iam.get_role(RoleName=role_name)
+        return response['Role']['Arn']
+    
+
+def delete_role(role_name):
+    role_def = iam.list_attached_role_policies(RoleName=role_name)
+    policies_list = iam.list_role_policies(RoleName=role_name)['PolicyNames']
+    
+    try:
+        for policy_name in role_def["AttachedPolicies"]:
+            iam.detach_role_policy(RoleName=role_name, PolicyArn=policy_name["PolicyArn"])
+        
+        for policy_name in policies_list:
+            iam.delete_role_policy(RoleName=role_name, PolicyName=policy_name)
+            #iam.delete_policy(PolicyArn=policy_name["PolicyArn"])
+        iam.delete_role(RoleName=role_name)
+        print(f'Deleted {role_name} successfully.')
+    except Exception as e:
+        print(f'Failed to delete {role_name}. {e}')
---
+++ glue-workflow/code/model_training_deployment.py
@@ -0,0 +1,173 @@
+import boto3
+import sys
+from datetime import datetime
+from awsglue.utils import getResolvedOptions
+
+class ModelRun:
+    def __init__(self, args):
+        args = getResolvedOptions(sys.argv, ['train_input_path', 'role_arn', 'algorithm_image'])
+        current_time = datetime.now()
+        self.train_input_path = args['train_input_path']
+        self.model_output_path = args['model_output_path']
+        self.algorithm_image = args['algorithm_image']
+        self.role_arn = args['role_arn']
+        timestamp_suffix = str(current_time.month) + "-" + str(current_time.day) + "-" + str(current_time.hour) + "-" + str(current_time.minute)
+        self.training_job_name = 'gw-xgb-churn-pred' + timestamp_suffix
+        self.endpoint = 'gw-xgb-churn-pred-endpoint' + timestamp_suffix
+    
+    def create_training_job(self):
+        print("Started training job...")
+        
+        try:
+            response = sagemaker.create_training_job(
+                TrainingJobName=self.training_job_name,
+                HyperParameters={
+                    'max_depth': '5',
+                    'eta': '0.2',
+                    'gamma': '4',
+                    'min_child_weight': '6',
+                    'subsample': '0.8',
+                    'silent': '0',
+                    'objective': 'binary:logistic',
+                    'num_round': '100',
+                    'eval_metric': 'auc'
+                },
+                AlgorithmSpecification={
+                    'TrainingImage': self.algorithm_image,
+                    'TrainingInputMode': 'File'
+                },
+                RoleArn=self.role_arn,
+                InputDataConfig=[
+                    {
+                        'ChannelName': 'train',
+                        'DataSource': {
+                            'S3DataSource': {
+                                'S3DataType': 'S3Prefix',
+                                'S3Uri': self.train_input_path + '/train',
+                                'S3DataDistributionType': 'FullyReplicated'
+                            }
+                        },
+                        'ContentType': 'text/csv',
+                        'CompressionType': 'None'
+                    },
+                    {
+                        'ChannelName': 'validation',
+                        'DataSource': {
+                            'S3DataSource': {
+                                'S3DataType': 'S3Prefix',
+                                'S3Uri': self.train_input_path + '/validation',
+                                'S3DataDistributionType': 'FullyReplicated'
+                            }
+                        },
+                        'ContentType': 'text/csv',
+                        'CompressionType': 'None'
+                    }
+                ],
+                OutputDataConfig={
+                    'S3OutputPath': self.model_output_path
+                },
+                ResourceConfig={
+                    'InstanceType': 'ml.m5.xlarge',
+                    'InstanceCount': 1,
+                    'VolumeSizeInGB': 20
+                },
+                StoppingCondition={
+                    'MaxRuntimeInSeconds': 86400
+                }
+            )
+            print("Training job has been created...")
+        except Exception as e:
+            print(e)
+            print('Unable to create training job')
+            raise(e)
+            
+    def describe_training_job(self):
+        status = sagemaker.describe_training_job(
+            TrainingJobName=self.training_job_name
+        )
+        print(self.training_job_name + " job status: ", status)
+        print("Waiting for " + self.training_job_name + " training job to complete...")
+        sagemaker.get_waiter('training_job_completed_or_stopped').wait(TrainingJobName=self.training_job_name)
+        resp = sagemaker.describe_training_job(TrainingJobName=self.training_job_name)
+        status = resp['TrainingJobStatus']
+        print("Training job " + self.training_job_name + " ended with status: " + status)
+        if status == 'Failed':
+            message = resp['FailureReason']
+            print('Training job {} failed with the following error: {}'.format(self.training_job_name, message))
+            raise Exception('Creation of sagemaker Training job failed')
+        return status
+            
+
+    def create_endpoint_config(self):
+
+        endpoint_name = self.endpoint
+
+        print("Creating model..")
+        create_model = sagemaker.create_model(
+            ModelName=endpoint_name,
+            PrimaryContainer=
+            {
+                'Image': self.algorithm_image,
+                'ModelDataUrl': self.model_output_path + f"/{self.training_job_name}/output/model.tar.gz"
+            },
+            ExecutionRoleArn=self.role_arn
+        )
+
+        resp = sagemaker.create_endpoint_config(
+            EndpointConfigName=endpoint_name,
+            ProductionVariants=[
+                {
+                    'VariantName': '{}-variant-1'.format(endpoint_name),
+                    'ModelName': endpoint_name,
+                    'InitialInstanceCount': 1,
+                    'InstanceType': 'ml.m5.large'
+                }
+            ])
+
+        print(resp)
+        return resp
+
+    def create_endpoint(self):
+        print("Creating endpoint..")
+        endpoint_name = self.endpoint
+        response = sagemaker.create_endpoint(
+            EndpointName=endpoint_name,
+            EndpointConfigName=endpoint_name
+        )
+        print(response)
+
+    def describe_endpoint(self):
+        status = sagemaker.describe_endpoint(EndpointName=self.endpoint)['EndpointStatus']
+        print(self.endpoint + "endpoint is now in status:", status)
+        print("Waiting for " + self.endpoint + " to be In-service...")
+        sagemaker.get_waiter('endpoint_in_service').wait(EndpointName=self.endpoint)
+        resp = sagemaker.describe_endpoint(EndpointName=self.endpoint)
+        status = resp['EndpointStatus']
+        print(self.endpoint + " endpoint is now in status:", status)
+        if status == 'Failed':
+            message = resp['FailureReason']
+            print('Test Endpoint {} creation failed with the following error: {}'.format(self.endpoint, message))
+            raise Exception('Endpoint creation failed')
+        return status
+        
+if __name__ == '__main__':
+
+    # Configure SDK to sagemaker
+    sagemaker = boto3.client('sagemaker')
+    s3 = boto3.resource('s3')
+    obj = ModelRun()
+
+    # Create training job
+    obj.create_training_job()
+
+    # Describe training job
+    status = obj.describe_training_job()
+
+    # Create endpoint conf
+    resp = obj.create_endpoint_config()
+
+    # Create endpoint for model
+    obj.create_endpoint()
+
+    # Describe endpoint
+    status = obj.describe_endpoint()
