3a46b4a4151a688a3bbc4892fdeec0bf346a65b0
guanghui0607 <wangguanghui0607@outlook.com>
2022-05-29 10:45:47 +0000

    fixed bugs
    1. Fixed error by joblib version
    2. Fixed error by the wrong directory of evaluation.json in S3 Bucket
    3. Added code for endpoint deployment

--- sagemaker-pipeline/code/evaluation.py
+++ sagemaker-pipeline/code/evaluation.py
@@ -1,55 +1,68 @@
 import json
 import os
 import tarfile
-
+import pathlib
 import pandas as pd
+import subprocess
 import numpy as np
-# subprocess.run("pip install xgboost", shell=True)
 
+subprocess.run("pip install xgboost", shell=True)
 import xgboost
-import glob
 
+#from sklearn.externals import joblib
 import joblib
-from sklearn.metrics import roc_auc_score, accuracy_score, precision_score, recall_score, confusion_matrix, roc_curve
+from sklearn.metrics import (
+    accuracy_score,
+    precision_score,
+    recall_score,
+    confusion_matrix,
+    roc_curve,
+)
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
 
 if __name__ == "__main__":
     model_path = os.path.join("/opt/ml/processing/model", "model.tar.gz")
     print("Extracting model from path: {}".format(model_path))
     with tarfile.open(model_path) as tar:
         tar.extractall(path=".")
-    print("Loading model")
+    
     model = joblib.load("xgboost-model")
-
-    print("Loading test input data")
-    test_path = "/opt/ml/processing/test/"
-    all_files = glob.glob(test_path + "/*.csv")
-    tmp = []
-    for filename in all_files:
-        df_tmp = pd.read_csv(filename, index_col=None, header=None)
-        tmp.append(df_tmp)
-    df = pd.concat(tmp, axis=0, ignore_index=True)
-
-    y_test = df.iloc[:, 0].values
-    df.drop(df.columns[0], axis=1, inplace=True)
-    X_test = xgboost.DMatrix(df.values)
+    test_data = os.path.join("/opt/ml/processing/test", "test.csv")
+    test_data_pd = pd.read_csv(test_data, header=None)
+    
+    y_test = test_data_pd.iloc[:, 0].to_numpy()
+    test_data_pd.drop(test_data_pd.columns[0], axis=1, inplace=True)
+    X_test = xgboost.DMatrix(test_data_pd.values)
     
     prediction_probabilities = model.predict(X_test)
     predictions = np.round(prediction_probabilities)
 
-    print("Creating classification evaluation report")
-    precision = precision_score(y_test, predictions, zero_division=1)
+    precision = precision_score(y_test, predictions)
     recall = recall_score(y_test, predictions)
     accuracy = accuracy_score(y_test, predictions)
     conf_matrix = confusion_matrix(y_test, predictions)
     fpr, tpr, _ = roc_curve(y_test, prediction_probabilities)
-    roc_auc = roc_auc_score(y_test, predictions)
-    # Available metrics to add to model: https://docs.aws.amazon.com/sagemaker/latest/dg/model-monitor-model-quality-metrics.html
+
+    logger.debug("Accuracy: {}".format(accuracy))
+    logger.debug("Precision: {}".format(precision))
+    logger.debug("Recall: {}".format(recall))
+    logger.debug("Confusion matrix: {}".format(conf_matrix))
+    
+    """
+    Need to customise the formation of evaluation.json to visualize the metrics in model registry based-on tasks
+    Result should coincide in format with the result by sagemaker.workflow.quality_check_step.QualityCheckStep.
+    Binarry
+    """
+    
     report_dict = {
         "binary_classification_metrics": {
             "accuracy": {"value": accuracy, "standard_deviation": "NaN"},
             "precision": {"value": precision, "standard_deviation": "NaN"},
             "recall": {"value": recall, "standard_deviation": "NaN"},
-            "roc_auc": {"value": roc_auc, "standard_deviation": "NaN"},
             "confusion_matrix": {
                 "0": {"0": int(conf_matrix[0][0]), "1": int(conf_matrix[0][1])},
                 "1": {"0": int(conf_matrix[1][0]), "1": int(conf_matrix[1][1])},
@@ -61,11 +74,10 @@ if __name__ == "__main__":
         },
     }
     
+    output_dir = "/opt/ml/processing/evaluation"
+    pathlib.Path(output_dir).mkdir(parents=True, exist_ok=True)
 
-    print("Classification report:\n{}".format(report_dict))
+    evaluation_path = f"{output_dir}/evaluation.json"
 
-    evaluation_output_path = os.path.join("/opt/ml/processing/evaluation", "evaluation.json")
-    print("Saving classification report to {}".format(evaluation_output_path))
-
-    with open(evaluation_output_path, "w") as f:
+    with open(evaluation_path, "w") as f:
         f.write(json.dumps(report_dict))
